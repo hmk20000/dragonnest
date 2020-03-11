792cee10ded7c616ccef25a2603341b4572b9ae7
smilejwj <61773785+smilejwj@users.noreply.github.com>
2020-03-11 20:24:27 +0900

    3/11
    - 폴더 생성
    - 스킬 sprite , 마나 sprite

---
+++ DragonNest/Assets/02 Script/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+
+    public void MagicSkillTree()
+    {
+
+    }
+}
+
+
---
+++ DragonNest/Assets/02 Script/StatusControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StatusControl : MonoBehaviour {
+
+
+    [SerializeField]
+    Text GoldPoint;
+    static int gold = 0;
+
+    [SerializeField]
+    Text ManaPoint;
+    static int mana = 0;
+
+    [SerializeField]
+    Text ArmyPoint;
+    static int army = 0;
+
+
+	void Start () {
+		
+        //로딩시 골드, 마나, 군사 정보 대입해야함.
+
+
+
+	}
+	
+	void Update () {
+
+        GoldPoint.text = gold.ToString();
+        ManaPoint.text = mana.ToString();
+        ArmyPoint.text = army.ToString();
+
+	}
+}
