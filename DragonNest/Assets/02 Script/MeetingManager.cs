f23fdfc0c90b2a8a92cac8fd18def2beb8da98cd
smilejwj <61773785+smilejwj@users.noreply.github.com>
2020-04-21 19:50:39 +0900

    버튼 랜덤 생성
    - 버튼위치 랜덤 (동굴 마크 주변은 제외)
    - 몬스터 배열 추가

--- DragonNest/Assets/02 Script/MeetingManager.cs
+++ DragonNest/Assets/02 Script/MeetingManager.cs
@@ -1,87 +1,143 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MeetingManager : MonoBehaviour {
 
+    // 조우 관리용 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     [SerializeField]
-    GameObject Meet;
-
+     GameObject Meet;
     [SerializeField]
-    GameObject BG;
-
+     GameObject BG;
     [SerializeField]
-    GameObject Charector;
-
+     GameObject Charector;
     [SerializeField]
-    Text Name;
+     Text Name;
+    [SerializeField]
+     GameObject ChoisBtn;
+    [SerializeField]
+    GameObject TextField;
 
 
-  
+    bool FadeOn = false;
+
+
+    static  int LV1Count = 7;
+
+
+    int MeetMin = 0; //랜덤 조우시, 몬스터 범위
+    int MeetMax = 7;
+
+
+    // 버튼 관리용 @@@@@@@@@@@@@@@@@@@@@@@@@@@
+    [SerializeField]
+    GameObject Canvas;
+    int MaxBtn = 10;
+    public GameObject MeetBtn;
 
 
 
     void Start () {
 		
+        for( int i = 0; i < MaxBtn; i++)
+        {
+           var A =  Instantiate(MeetBtn);
+            A.transform.SetParent(Canvas.transform);
+
+            // 버튼 위치 랜덤 생성
+            A.transform.localPosition = new Vector3(UnityEngine.Random.Range(-100, 100), UnityEngine.Random.Range(-200, 200), 0);
+
+
+            // 동굴 위치가 안 가리게
+            if(A.transform.position.x > -20 && 
+                A.transform.position.x < 20 &&
+                A.transform.position.y > -20 &&
+                A.transform.position.y < 20)
+            {
+                A.transform.position = new Vector3( A.transform.position.x * 2, 
+                                                    A.transform.position.y * 2, 0);
+
+            }
+            
+        }
+
+
 	}
 	
 	void Update () {
 		
+        if( FadeOn)
+        {
+            Meet.GetComponent<Image>().color        += new Color(0, 0, 0, 1f * Time.deltaTime);
+            TextField.GetComponent<Image>().color   += new Color(0, 0, 0, 1f * Time.deltaTime);
+            ChoisBtn.GetComponent<Image>().color    += new Color(0, 0, 0, 1f * Time.deltaTime);
+            BG.GetComponent<Image>().color          += new Color(0, 0, 0, 1f * Time.deltaTime);
+            
+        }
+
+        if (BG.GetComponent<Image>().color.a > 0.9) 
+        {
+            Charector.GetComponent<Image>().color   += new Color(0, 0, 0, 1f * Time.deltaTime);
+            Name.color                              += new Color(0, 0, 0, 1f * Time.deltaTime);
+        }
+
 	}
 
 
 
 
     // 조우 버튼용 함수 
-    public void Meeting( int no)
+    public void Meeting( )
     {
+        int mon = UnityEngine.Random.Range(MeetMin, MeetMax);
 
-        switch(no)
-        {
-            case 0: //핑크
-                SetMeeting(GetComponent<MonsterManager>().Pink);
-                break;
 
-            case 1: //옐로
-                SetMeeting(GetComponent<MonsterManager>().Yellow);
-                break;
-
-            case 2: //블랙
-                SetMeeting(GetComponent<MonsterManager>().Choco);
-                break;
-
-            case 3: //고블린
-                SetMeeting(GetComponent<MonsterManager>().Goblin);
-                break;
-
-            case 4: //오크
-                SetMeeting(GetComponent<MonsterManager>().Ork);
-                break;
-        }
-
+        SetMeeting(mon);
 
         //조우 화면 중앙
         Meet.transform.localPosition = new Vector3(0, -20, 0);
 
+        FadeOn = true;
+        
     }
 
-    public void SetMeeting(Monster _target)
+
+    //
+    public void SetMeeting(int _mon)
     {
+        Monster _target = GetComponent<MonsterManager>().A[_mon];
+
         //조우 화면 변경  
-        BG.GetComponent<Image>().sprite = GetComponent<MonsterManager>().BGImg[_target.BG];
-        Charector.GetComponent<Image>().sprite = GetComponent<MonsterManager>().MonImg[_target.no];
-        Charector.transform.localScale = new Vector3(_target.size, _target.size, _target.size);
-        Name.text = _target.name;
+        BG.GetComponent<Image>().sprite         = GetComponent<MonsterManager>().BGImg[_target.GetBackGroundNum()];
+        Charector.GetComponent<Image>().sprite  = GetComponent<MonsterManager>().MonImg[_target.GetImageNum()];
+        Charector.transform.localScale          = new Vector3(_target.GetSize(), _target.GetSize());
+        Name.text                               = _target.GetName();
 
 
 
     }
 
+
+    // 플레이어의 선택 후
     public void ChoiceButton()
     {
-        //조우 화면 중앙
+
+
+        //조우 바깥으로 이동
         Meet.transform.localPosition = new Vector3(1000, -20, 0);
+
+        Meet.GetComponent<Image>().color        = new Color(0, 0, 0, 0);
+        TextField.GetComponent<Image>().color   = new Color(0, 0, 0, 0);
+        ChoisBtn.GetComponent<Image>().color    = new Color(0, 0, 0, 0);
+        BG.GetComponent<Image>().color          = new Color(0, 0, 0, 0);
+        Charector.GetComponent<Image>().color   = new Color(0, 0, 0, 0);
+        Name.color                              = new Color(0, 0, 0, 0);
+
+        SceneManager.LoadScene("Main");
+
     }
 }
 
--- DragonNest/Assets/02 Script/MonsterManager.cs
+++ DragonNest/Assets/02 Script/MonsterManager.cs
@@ -7,24 +7,33 @@ using UnityEngine;
 public class Monster
 {
     //생성자
-    public Monster(string _name, int _lv, int _bg, int _image, float _size )
+    public Monster( string _name,   // 몬스터 이름
+                    int _lv,        // 레벨
+                    int _BackGroungNum,        // 배경 번호
+                    int _ImageNum,     // 이미지 번호
+                    float _size     // 크기 (비율)
+                  ) 
+
     {
         name = _name;
         level = _lv;
-        BG = _bg;
-        no = _image;
+        BG = _BackGroungNum;
+        no = _ImageNum;
         size = _size;
     }
 
-    public string name;
-    public int level = 0;
-
-    public int BG = 0;          //이미지 넘버링 (enum 사용)
-
-    public int no = 0;          //이미지 넘버링 (enum 사용)
-    public float size = 1;      //이미지 크기 비율
+    readonly string name;
+    readonly int level = 0;
+    readonly int BG = 0;          //이미지 넘버링 (enum 사용)
+    readonly int no = 0;          //이미지 넘버링 (enum 사용)
+    readonly float size = 1;      //이미지 크기 비율
 
 
+    public string GetName() { return name; }
+    public int GetLv() { return level; }
+    public int GetBackGroundNum() { return BG; }
+    public int GetImageNum() { return no; }
+    public float GetSize() { return size; }
 }
 
 public class MonsterManager : MonoBehaviour {
@@ -38,27 +47,54 @@ public class MonsterManager : MonoBehaviour {
     }
 
     // --------------------------------------- 몬스터 정보 ---------------------------------------
-   public  Sprite[] MonImg;
+    public Sprite[] MonImg;
 
-   public enum MonNo
+    public enum MonNo
     {
-        PINK ,
+        PINK, //0
         YELLOW,
         CHOCO,
         GOBLIN,
-        ORK
+        ZOMBIE,
+        MUMY, //5
+        ORK,
+        SKELETON
+
     }
 
 
 
-    public Monster Pink    = new Monster("딸기맛 \n 핑크 슬라임",1, (int)Field .Forest  , (int)MonNo.PINK, 1);
-    public Monster Yellow  = new Monster("바나나맛 \n 옐로우 슬라임",1, (int)Field .Forest  , (int)MonNo.YELLOW, 1);
-    public Monster Choco   = new Monster("쪼꼬맛 \n 다크 슬라임",1, (int)Field .Forest  , (int)MonNo.CHOCO, 0.8f);
-    public Monster Goblin  = new Monster("키위 \n 고블린",1, (int)Field .Forest  , (int)MonNo.GOBLIN, 1);
-    public Monster Ork     = new Monster("숯불구이 \n 오크",1, (int)Field .Forest  , (int)MonNo.ORK, 1.5f);
+
+    //몬스터 배열
+    public Monster[] A = {
+
+     new Monster("딸기맛\n슬라임",     1, (int)Field.Forest, (int)MonNo.PINK, 1),
+     new Monster("바나나맛\n슬라임", 1, (int)Field.Forest, (int)MonNo.YELLOW, 1),
+     new Monster("쪼꼬맛\n슬라임",     1, (int)Field.Forest, (int)MonNo.CHOCO, 0.8f),
+     new Monster("키위\n고블린",            1, (int)Field.Forest, (int)MonNo.GOBLIN, 1),
+     new Monster("수박\n좀비",              1, (int)Field.Forest, (int)MonNo.ZOMBIE, 1.5f),
+     new Monster("미숫가루\n미이라",        1, (int)Field.Forest, (int)MonNo.MUMY, 1.5f),
+        //황금 고블린 ++
+
+     new Monster("숯불구이\n오크",          2, (int)Field.Forest, (int)MonNo.ORK, 1.5f),
+     new Monster("사골\n스켈레톤",          2, (int)Field.Forest, (int)MonNo.SKELETON, 1.5f)
+
+};
 
 
-    
+    //public Monster Pink    = new Monster("딸기맛\n핑크 슬라임",       1, (int)Field .Forest  , (int)MonNo.PINK, 1);
+    //public Monster Yellow  = new Monster("바나나맛\n옐로우 슬라임",   1, (int)Field .Forest  , (int)MonNo.YELLOW, 1);
+    //public Monster Choco   = new Monster("쪼꼬맛\n다크 슬라임",       1, (int)Field .Forest  , (int)MonNo.CHOCO, 0.8f);
+    //public Monster Goblin  = new Monster("키위\n고블린",              1, (int)Field .Forest  , (int)MonNo.GOBLIN, 1);
+    //public Monster Zombie  = new Monster("수박\n좀비" ,               1, (int)Field .Forest  , (int)MonNo.ZOMBIE, 1.5f);
+    //public Monster Mumy    = new Monster("미숫가루\n미이라",          1, (int)Field.Forest   , (int)MonNo.MUMY, 1.5f);
+    ////황금 고블린 ++
+
+    //public Monster Ork     = new Monster("숯불구이\n오크",            2, (int)Field .Forest  , (int)MonNo.ORK, 1.5f);
+    //public Monster Skeleton= new Monster("사골\n스켈레톤" ,           2, (int)Field .Forest  , (int)MonNo.SKELETON, 1.5f);
+
+
+
 
 
 
